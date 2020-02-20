190ae8d3d9ee9eb4c56c0e3af210f193893d9207
Javier Nogales <javi@MacBook-Air-de-Noemi.local>
2020-02-20 06:25:34 +0100

    Devices model

--- lib/features/domain/entities/default_trainer_device.dart
+++ lib/features/domain/entities/default_trainer_device.dart
@@ -4,9 +4,7 @@ import 'package:trainerapp/features/domain/entities/device.dart';
 import 'package:trainerapp/features/domain/entities/trainer_device.dart';
 
 class DefaultTrainerDevice extends TrainerDevice{
-
-  final Device _device;
-
-  DefaultTrainerDevice(this._device);
+  
+  DefaultTrainerDevice(Device device) : super(device);  
 
 }
--- lib/features/domain/entities/trainer_device.dart
+++ lib/features/domain/entities/trainer_device.dart
@@ -1,7 +1,11 @@
 
 
+import 'package:trainerapp/features/domain/entities/device.dart';
+
 abstract class TrainerDevice {
 
-  
+  final Device _device;
+
+  TrainerDevice(this._device);
 
 }
