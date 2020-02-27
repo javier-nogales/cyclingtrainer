115e9e9a5c030285a643d699188bae8b086b72f6
Javier Nogales <javi@MacBook-Air-de-Noemi.local>
2020-02-27 07:18:14 +0100

    wip: stream test

--- lib/features/domain/repositories/devices_repository.dart
+++ lib/features/domain/repositories/devices_repository.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter_blue/flutter_blue.dart';
 
-abstract class DeviceRepositoryBase {
+abstract class DevicesRepository {
 
   final FlutterBlue _flutterBlue = FlutterBlue.instance;
 
--- lib/features/domain/repositories/trainer_device_repository.dart
+++ lib/features/domain/repositories/trainer_device_repository.dart
@@ -2,9 +2,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:trainerapp/core/error/failures.dart';
 import 'package:trainerapp/features/domain/entities/trainer_device.dart';
+import 'package:trainerapp/features/domain/repositories/devices_repository.dart';
 
 abstract class TrainerDeviceRepository {
 
   Either<Failure, Stream<TrainerDevice>> getTrainerDevice();
+
+
 
 }
