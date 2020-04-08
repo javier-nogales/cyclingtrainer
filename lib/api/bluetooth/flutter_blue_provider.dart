2eb7ebf63a60739583cf9c199bb8b189b8177286
javier-nogales <javier.nog76@gmail.com>
2020-04-08 19:39:32 +0200

    api 0.0.3+1

--- lib/api/bluetooth/flutter_blue_provider.dart
+++ lib/api/bluetooth/flutter_blue_provider.dart
@@ -1,22 +1,46 @@
 
+import 'package:rxdart/rxdart.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:trainerapp/api/bluetooth/bt_device.dart';
-import 'package:trainerapp/api/bluetooth/flutter_blue_device.dart';
 import 'package:trainerapp/api/bluetooth/bluetooth_provider.dart';
+import 'package:trainerapp/api/bluetooth/flutter_blue_device.dart';
 
 class FlutterBlueProvider implements BluetoothProvider {
 
-  FlutterBlue _flutterBlue = FlutterBlue.instance;
+  final _flutterBlue = FlutterBlue.instance;
+  final _btDevicesStream = BehaviorSubject<List<BTDevice>>();
+
+  FlutterBlueProvider() {
+    var subscription = _flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      for (ScanResult r in results) {
+        print('${r.device.name} found! rssi: ${r.rssi}');
+      }
+    });
+
+//    _flutterBlue.scanResults.listen((scanResults) {
+//      print('Listen results');
+//      List<BTDevice> outList = [];
+//      print('2');
+//      scanResults.forEach((result) {
+//        print('single result = ${result.device.id}');
+//        final btDevice = FlutterBlueDevice(result.device);
+//        outList.add(btDevice);
+//      });
+//      _btDevicesStream.add(outList);
+//    });
+  }
 
   @override
   Stream<List<BTDevice>> fetchAllDevices() {
-    _flutterBlue.scan(timeout: Duration(seconds: 3));
-    Stream<List<BTDevice>> outList = _flutterBlue.scanResults.asyncMap(
-            (List<ScanResult> results) {
-              return results.map((r) => FlutterBlueDevice(r.device)).toList();
-            }
-    );
-    return outList;
+    _flutterBlue.startScan(timeout: Duration(seconds: 3));
+    return _btDevicesStream;
+//    Stream<List<BTDevice>> outList = _flutterBlue.scanResults.asyncMap(
+//            (List<ScanResult> results) {
+//              return results.map((r) => FlutterBlueDevice(r.device)).toList();
+//            }
+//    );
+//    return outList;
   }
 
   @override
---
+++ lib/injection_container.dart
@@ -0,0 +1,28 @@
+
+import 'package:get_it/get_it.dart';
+import 'package:trainerapp/api/bluetooth/flutter_blue_provider.dart';
+import 'package:trainerapp/api/use_cases/bluetooth_controller.dart';
+
+import 'api/bluetooth/bluetooth_provider.dart';
+import 'api/use_cases/bluetooth_use_cases.dart';
+import 'bloc/bluetooth_scan/bluetooth_scan_bloc.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+
+  sl.registerFactory(
+    () => BluetoothScanBloc(
+      useCases: sl()
+    )
+  );
+
+  sl.registerLazySingleton<BluetoothUseCases>(
+    () => BluetoothController(sl())
+  );
+
+  sl.registerLazySingleton<BluetoothProvider>(
+      () => FlutterBlueProvider()
+  );
+
+}
